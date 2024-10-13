6a93ef9fe85652279386c586281cc057b77f5d4f
georgesohran <soxrgeorge@gmail.com>
2024-10-13 20:50:57 +0500

    changing the way dock is positioned

--- components/DockBar.jsx
+++ components/DockBar.jsx
@@ -6,57 +6,46 @@ import { FaReact } from "react-icons/fa";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 
-const NavButtonBig = () => {
-
+const DockBackground = (params) => {
+  if(!('isTop' in params)) {
+    // some error handline idk
+  }
+  return (
+    <div style={{
+      bottom: params.isTop ? null : 0,
+      top: params.isTop ? 0 : null
+    }} className="h-5 z-0 fixed bottom-0 left-0 right-0
+      rounded-full border-2 border-slate-500 bg-slate-900">
+    </div>
+  )
 }
 
-
 export default function DockBar(params) {
-  const [offset, setOffset] = useState(null)
-  const [topPos, setTopPos] = useState(false)
-  const ref = useRef(null)
-
-  // handling the change on window size
-  const onWindowResize = useCallback(event => {
-    setOffset((window.innerWidth-ref.current.offsetWidth) / 2)
-  })
-  useEffect(() => {
-    window.addEventListener('resize', onWindowResize)
-    onWindowResize()
-    return () => window.removeEventListener('resize', onWindowResize)
-  }, [onWindowResize])
+  const [isTop, setIsTop] = useState(false)
 
   return (
-    <div ref={ref} className="absolute flex flex-row gap-2" style={{
-      right: offset,
-      bottom: topPos ? null : 16,
-      top: topPos ? 16 : null
-    }}>
-      <div className="p-3 flex flex-row gap-2 
-        rounded-full text-4xl
-        border-2 border-white">
-        <div className="hover:text-5xl grow self-end transition-all">
-          <FaReact/>
-        </div>
-        <div className="hover:text-5xl grow self-end transition-all ">
-          <FaReact/>
-        </div>
-        <div className="hover:text-5xl grow self-end transition-all ">
-          <FaReact/>
-        </div >
-        <div className="hover:text-5xl grow self-end transition-all ">
-          <FaReact/>
-        </div>
+    <div className={`absolute w-full text-center text-4xl ${isTop? 'top-3' : 'bottom-3'}`} >
+      {/* <DockBackground isTop={isTop}/> */}
+      {/* dock icons */}
+      <div className={`inline-block z-10 mx-1 p-1 
+        rounded-full bg-slate-800 hover:text-5xl hover:bg-slate-700 transition-all  
+        ${isTop ? 'align-top' : 'align-bottom'}`}>
+        <FaReact/>
       </div>
-      <div className="p-2 flex flex-row gap-2
-        rounded-full text-2xl my-2
-        border-2 border-white">
-        <div>
-          <IoMdSettings/>
-        </div>
-        <div>
-          <IoMdSettings/>
-        </div>
+      <div className={`inline-block z-10 mx-1 p-1 
+        rounded-full bg-slate-800 hover:text-5xl hover:bg-slate-700 transition-all  
+        ${isTop ? 'align-top' : 'align-bottom'}`}>
+        <FaReact/>
+      </div>
+      <div className={`inline-block z-10 mx-1 p-1 
+        rounded-full bg-slate-800 hover:text-5xl hover:bg-slate-700 transition-all  
+        ${isTop ? 'align-top' : 'align-bottom'}`}>
+        <FaReact/>
+      </div >
+      <div className={`inline-block z-10 mx-1 p-1 
+        rounded-full bg-slate-800 hover:text-5xl hover:bg-slate-700 transition-all  
+        ${isTop ? 'align-top' : 'align-bottom'}`}>
+        <FaReact/>
       </div>
     </div>
   )
