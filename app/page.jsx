225883cab480c9979e464b446b03c41649bc0203
georgesohran <soxrgeorge@gmail.com>
2024-10-13 20:36:12 +0500

    starting work on main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <div className='bg-red-300'>hello ah</div>
+      <div className="absolute w-96 h-96 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        bg-gradient-to-br from-slate-900 from-20% 
+        via-slate-800 via-40% to-slate-900 to-60% rounded-xl">
+        <div className=""></div>
+      </div>
     </div>
   );
 }
